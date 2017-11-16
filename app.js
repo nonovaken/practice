cadadf18de828f66b8c8794b6a326ffd9cb3ee4c
Radiomad <tim.yuduoguo@gmail.com>
2017-11-15 21:21:57 -0800

    connect node server with react client

--- app.js
+++ app.js
@@ -22,8 +22,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+    next();
+});
+
+app.use('/', (req, res) => {
+
+});
+
+app.use('/click', (req, res) => {
+    res.json({content: 'Hello World!'})
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: null
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick() {
+        axios.get('http://localhost:3001/click')
+            .then((res) => {
+                this.setState({
+                    content: res.data.content
+                })
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    render() {
+        let display = null;
+        if(this.state.content) {
+            display = <div>{this.state.content}</div>;
+        } else {
+            display = <button onClick={this.handleClick}>CLICK</button>;
+        }
+        return (
+          <div className="App">
+              {display}
+          </div>
+        );
+    }
 }
 
 export default App;
